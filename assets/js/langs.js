a15d392ea7e3c0559d630721bf305d0e6f0f1fb1
Tarek Tarabichi <ttarabichi@gmail.com>
2025-06-19 21:54:56 +0400

    Use flag-icons classes for languages

--- assets/js/langs.js
+++ assets/js/langs.js
@@ -1,14 +1,14 @@
 window.availableLanguages = {
-	en: { label: "English", flag: "🇬🇧", dir: "ltr" },
-	fr: { label: "Français", flag: "🇫🇷", dir: "ltr" },
-	de: { label: "Deutsch", flag: "🇩🇪", dir: "ltr" },
-	es: { label: "Español", flag: "🇪🇸", dir: "ltr" },
-	pt: { label: "Português", flag: "🇵🇹", dir: "ltr" },
-	id: { label: "Bahasa Indonesia", flag: "🇮🇩", dir: "ltr" },
-	ar: { label: "العربية", flag: "🇸🇦", dir: "rtl" },
-	tl: { label: "Tagalog", flag: "🇵🇭", dir: "ltr" },
-	hi: { label: "हिन्दी", flag: "🇮🇳", dir: "ltr" },
-	ur: { label: "اردو", flag: "🇵🇰", dir: "rtl" },
-	
-	// Add new languages here
+  en: { label: "English", code: "gb", dir: "ltr" },
+  fr: { label: "Français", code: "fr", dir: "ltr" },
+  de: { label: "Deutsch", code: "de", dir: "ltr" },
+  es: { label: "Español", code: "es", dir: "ltr" },
+  pt: { label: "Português", code: "pt", dir: "ltr" },
+  id: { label: "Bahasa Indonesia", code: "id", dir: "ltr" },
+  ar: { label: "العربية", code: "sa", dir: "rtl" },
+  tl: { label: "Tagalog", code: "ph", dir: "ltr" },
+  hi: { label: "हिन्दी", code: "in", dir: "ltr" },
+  ur: { label: "اردو", code: "pk", dir: "rtl" },
+
+  // Add new languages here
 };
