6c4b57f66d2c3b2d67105a4cac2dd7ee2c60aac0
Tarek Tarabichi <ttarabichi@gmail.com>
2025-06-30 11:13:40 +0400

    Add configurable email output path

--- includes/config/settings.php
+++ includes/config/settings.php
@@ -1,8 +1,10 @@
 <?php
-	return [
-		'SSLEnabled' => 1,
-		'Port'       => 443,
-		'ProjectPath' => 'D:/laragon/www/',  // <- Adjust to YOUR project base directory
-		'IgnoreDirs' => ['.', '..', 'includes', 'modules', '.idea', 'logs', 'vendor', 'assets'],
-	];
+        return [
+                'SSLEnabled' => 1,
+                'Port'       => 443,
+                'ProjectPath' => 'D:/laragon/www/',  // <- Adjust to YOUR project base directory
+                'IgnoreDirs' => ['.', '..', 'includes', 'modules', '.idea', 'logs', 'vendor', 'assets'],
+                // Path where Laragon stores outgoing emails
+                'email_output_path' => 'D:/laragon/bin/sendmail/output/',
+        ];
 ?>
